38c5e63d799342add62e4bad0fbfbe9491f00de5
fttasci <44835275+fttasci@users.noreply.github.com>
2018-11-08 15:21:52 +0300

    Yeni proje

---
+++ src/test/java/myclass.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class myclass {
+
+    public static void main(String[] args) {
+        String email = "fatih.tasci.optiim@gmail.com";
+        String sifre = "karakartal25";
+
+        System.setProperty("driver", "C:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.n11.com");
+        driver.manage().window().maximize();
+        WebElement element = driver.findElement(By.className("btnSignIn"));
+        element.click();
+        WebElement element2 = driver.findElement(By.id("email"));
+        element2.sendKeys(email);
+        WebElement element3 = driver.findElement(By.id("password"));
+        element3.sendKeys(sifre);
+        driver.findElement(By.id("loginButton")).click();
+
+       /* driver.findElement(By.cssSelector("[class='myAccount'] [class='menuTitle ']")).click();
+        driver.findElement(By.cssSelector("[class='hOpenMenuContent'] [class='logoutBtn']")).click(); */
+
+
+    }
+
+
+
+}
